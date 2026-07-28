35ae5c1b0503fa4b445efe50477d116c47580183
miguel olegna <miguel.olegna1@gmail.com>
2026-07-29 00:08:18 +0100

    variaveis de ambiente

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,11 @@
 // src/services/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cmhyvqaqpiuycjhohjto.supabase.co';
-const supabaseKey = 'sb_publishable_0mkA2w67oCR2wdvgHuFvGw_b9nZ4xB2';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Anon Key must be defined in environment variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
